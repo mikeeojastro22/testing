b0a8049a2796376591dd3823e5dd1372e6c70caf
Mikee Ojastro <mikeeojastro@gmail.com>
2024-11-20 19:39:09 +0800

    November 20, 2024

---
+++ src/components/Counter.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Counter from "./Counter";
+
+test("increment button", () => {
+
+});
+
+test("decrement button", () => {
+
+});
